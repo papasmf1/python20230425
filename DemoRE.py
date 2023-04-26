55069a057f1bfa586ec9bbc885d4ca11c5ba0dec
papasmf1 <papasmf1@gmail.com>
2023-04-26 10:26:45 +0900

    정규표현식 연습1

---
+++ DemoRE.py
@@ -0,0 +1,19 @@
+# DemoRE.py 
+import re 
+
+#함정을 추가
+result = re.search("[0-9]*th", "  35th")
+print(result)
+print(result.group())
+
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
+
+result = re.search("apple", "this is apple")
+print(result.group())
+result = re.search("\d{4}", "올해는 2023년입니다.")
+print(result.group())
+result = re.search("\d{5}", "우리 동네는 52300")
+print(result.group())
+
